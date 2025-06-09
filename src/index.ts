0d4d0bb8487b285d87e3ce58539e8988b88b3004
Tiago <tiagosantinidasilva@gmail.com>
2025-06-09 18:19:50 -0300

    ESTRUTURA

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes/index';
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use('/api', routes);
+
+mongoose.connect('mongodb://localhost:27017/finance-control')
+  .then(() => {
+    console.log('MongoDB conectado');
+    app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
+  })
+  .catch(err => console.error('Erro na conexão MongoDB:', err));
